00d36269e363ba118ecd4e9d9fce9c61526e2473
tanyavoid <tanyavoid@ya.ru>
2021-04-28 22:20:04 +0300

    Formatting change

--- gotchas/floats_imprecision.py
+++ gotchas/floats_imprecision.py
@@ -1,14 +1,17 @@
-# https://docs.python.org/3/tutorial/floatingpoint.html
-# "most decimal fractions cannot be represented exactly as binary fractions"
+"""
+"most decimal fractions cannot be represented exactly as binary fractions"
+https://docs.python.org/3/tutorial/floatingpoint.html
+"""
 
 from decimal import Decimal
 
-print(f'0.1 * 3\n>>> {0.1 * 3}')
-print(f'0.1 * 3 == 0.3\n>>> {0.1 * 3 == 0.3}')
+print('>>> 0.1 * 3')
+print(0.1 * 3)
+print('>>> 0.1 * 3 == 0.3')
+print(0.1 * 3 == 0.3)
 
 # decimal module provides the exact decimal representation
 # suitable when high precision is required
 d = Decimal(0.1)
-print(f'Decimal(0.1)\n>>> {d}')
-
-# a simpler solution is to use integers (like cents or milliseconds)
+print('>>> Decimal(0.1)')
+print(d)
