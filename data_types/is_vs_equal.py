3a0f410f9c129139b02dfb11a512903d69416735
tanyavoid <tanyavoid@ya.ru>
2021-05-25 21:18:39 +0300

    Fix formatting

--- data_types/is_vs_equal.py
+++ data_types/is_vs_equal.py
@@ -2,23 +2,26 @@
 "Every object has an identity, a type and a value."
 https://docs.python.org/3/reference/datamodel.html
 
-identity - an object’s address in memory
-is operator compares the identity of two objects
-== operator compares values of objects (the data they hold)
+Identity — an object’s address in memory;
+is operator compares the identity of two objects,
+== operator compares values of objects (the data they hold).
 """
 
 a = [0, 1, 2, 3, 4]
 b = [0, 1, 2, 3, 4]
 c = a
 
-print(a == b)  # equal values
-print(a is b)
-print(a == c)
-print(a is c)  # equal identities
+a is c  # equal identities
+a == b  # equal values
+
+assert a is c
+assert a is not b
+assert a == c
+assert a == b
 
 # equal memory addresses mean the variables point to the same object
-print(id(a) == id(b))
-print(id(a) == id(c))
+id(a) == id(c)  # True, the same object in memory
+id(a) == id(b)  # False
 
 # the only common proper case of "is" is checking for None
 if a is None:
