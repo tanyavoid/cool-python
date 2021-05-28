f7105d5aed6c1384fba955f04d74d84a84cc7056
tanyavoid <tanyavoid@ya.ru>
2021-05-28 22:31:23 +0300

    Minor edits

--- functions/function_intro.py
+++ functions/function_intro.py
@@ -1,23 +1,22 @@
 """
-Functions are reusable pieces of code created to do a specific task.
+Functions are useful reusable pieces of code created to do a specific task.
 
-They are defined (created) using def keyword and giving them a name.
+They are defined (created) using *def* keyword following by a name.
 The name should be descriptive and short, staring with a verb, if possible.
-Then a function can be called (used) any number of times to do a task.
+Then a function can be called (used) any number of times to do its task.
 
-Sometimes functions are compared to black boxes — they do some stuff, but it's
+Sometimes functions are compared to black boxes — they do their thing, but it's
 not necessary to know how it's done.
 """
 
 
 def do_nothing():
-    """This function does nothing."""
-    pass
+    """This function does nothing, meh."""
 
 
 def return_nothing():
     """In most cases functions *return* a result. But by default it's None."""
-    'Bloop'
+    print('Doin some stuff but return nothing.')
 
 
 def make_noise():
@@ -25,7 +24,7 @@ def make_noise():
 
 
 def make_sound(sound='Bloop'):
-    """print() doesn't replace return statement; it just has side effect.
+    """print() doesn't replace return statement; it just has a side effect.
     return must be the last line in a function; anything after that is ignored.
     """
     print(sound)
@@ -34,9 +33,12 @@ def make_sound(sound='Bloop'):
 
 
 if __name__ == '__main__':
-    # calling (using) a function and saving its result in a variable
+    # calling a function and saving its result in a variable
     nothing = return_nothing()
-    assert nothing is None  # but we knew
+    assert nothing is None
+
+    noise = make_noise()
+    assert noise is not None
 
     sound = make_sound()
     assert sound is not None
