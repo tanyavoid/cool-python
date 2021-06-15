4a216d3e57680dd704e9c234635900038fca2b33
tvey <tanyavoid@ya.ru>
2021-06-15 22:21:32 +0300

    Parameters-arguments

--- functions/parameters_arguments.py
+++ functions/parameters_arguments.py
@@ -1,21 +1,15 @@
 """
-Function becomes ever more useful when we can pass to it some pieces of
-information which can influence its result or change the information.
+Functions become ever more useful when we can pass to them some information.
 
-This information is called function parameters (or arguments).
-Parameters are what's specified on a function definition,
-arguments — what is passed on a function call.
-But in general the two words are used interchangeably.
+Creating a function, we specify the expected information with parameters.
+On a function call we pass arguments that match these parameters.
+(In general words “parameters” and “arguments” can be used interchangeably.)
 
-Positional arguments and mandatory, keyword arguments are not.
-Positional arguments always go before keyword arguments.
-Keyword arguments have default values.
-
-“args” and “kwargs” names are conventions.
-Asterisk (*) in Python performs unpacking:
-single * — list (or tuple) unpacking,
-double ** — dict unpacking.
-
+There are 4 ways to pass arguments to functions:
+- positional arguments (mandatory, and their order matters),
+- keyword arguments (named args that have default values),
+- argument list/tuple (usually called *args),
+- argument dictionary (**kwargs).
 """
 
 
@@ -23,24 +17,48 @@ def get_everything(*args, **kwargs):
     """Explore the way parameters look in a function.
 
     This function takes any number of positional and keyword arguments.
+    “args” and “kwargs” variable names are conventions and can be replaced
+    with other names.
     """
     assert isinstance(args, tuple)
     assert isinstance(kwargs, dict)
-    print(locals())  # all the args and kwargs
-    print(args)
-    print(kwargs)
+    print(locals())  # args and kwargs are keys in the locals() dict
+    print('args:', args)
+    print('kwargs:', kwargs)
 
 
 def get_nothing():
-    print(locals())  # such empty
+    assert not locals()  # such empty
 
 
-# Function parameters must be in a specific order
-def do_thing(positional, keyword=None, *args, **kwargs):
+def get_args(positional, keyword=None, *args, **kwargs):
+    """Function parameters must be in a specific order.
+
+    Any other way to define and pass them will result in SyntaxError.
+    """
     pass
 
 
-if __name__ == '__main__':
-    basket = ['cherries', 'blueberries', 'strawberries', 'gooseberries']
+def add_participant(first_name, last_name):
+    """
+    Positional arguments are used when there are few of them required.
 
-    get_everything(1992, 'otter', color='magenta', age=None, *basket)
+    Their order is natural and therefore easy to remember.
+    """
+    return f'{first_name} {last_name} is added to a list.'
+
+
+def collect_info(first_name, last_name, house=None, patronus=None):
+    """
+    When more information needed, order of arguments is harder to remember.
+
+    And some pieces of information may not be mandatory. 
+    In this case keyword arguments come in handy.
+    They are expected (opposite to arbitrary **kwargs) but not required.
+    """
+    if house == 'Slytherin' and patronus:
+        print('No way')
+
+
+if __name__ == '__main__':
+    get_everything(1979, 'otter', color='violet', occupation='dentist')
